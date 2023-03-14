23a65a8ea0aa05598412d932c126ca7c153e8c90
Delfor Fuente <delforfuente@gmail.con>
2023-03-14 19:35:28 -0300

    new photos added on "WRC", scrollbar and js modification

--- js/js.js
+++ js/js.js
@@ -1,35 +1,3 @@
-let cad = `
-<nav>
-<input type="checkbox" name="show-menu" id="show-menu">
-<label for="show-menu" class="menu-icon"><i class="fa-solid fa-bars"></i></label>
-<div class="content">
-    <div class="logo"><a href="index.html">FACUNDO AHUMADA PH</a></div>
-    <ul class="links">
-        <li class="links"><a href="#Dakar">Rally Dakar</a></li>
-        <li class="links"><a href="#WRC">WRC</a></li>
-        <li class="links"><a href="#RallyArg">Rally argentino</a></li>
-        <li class="links"><a href="#Other">Other sports</a></li>
-        <li class="links"><a href="#Portait">Portait</a></li>
-        <li class="links"><a href="#">About me</a></li>
-        <li class="links"><a href="#Contact">Contact</a></li>
-    </ul>
-</div>
-</nav>
-`
-
-document.getElementById("head").innerHTML = cad;
-
-cad = `
-<span class="copy">
-    Delfor Fuente - 2022 | Todos los derechos reservados.
-</span>
-<ul class="social">
-    <li><a href="mailito:rcfacundo@gmail.com">rcfacundo@gmail.com</a></li>
-    <li><a href="https://www.instagram.com/facundo_ahumada_fotografia/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-</ul>
-`
-document.getElementById("final").innerHTML = cad;
-
 // MODAL
 document.querySelectorAll('.container img').forEach(image =>{
     image.onclick = () =>{
