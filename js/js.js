4e6cc12892d6c54baf78d71676d80723f9282ed2
Delfor Fuente <delforfuente@gmail.con>
2023-03-17 10:00:17 -0300

    need to make the form run

--- js/js.js
+++ js/js.js
@@ -26,3 +26,18 @@ function topFunction(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+//HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
